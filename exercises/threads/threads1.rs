e37d08891dbba14291bcc074885626a735ca6a2d
oda3174964 <yohasakura@126.com>
2022-03-23 22:42:49 +0800

    thread1 mutex

--- exercises/threads/threads1.rs
+++ exercises/threads/threads1.rs
@@ -9,23 +9,28 @@
 // I AM NOT DONE
 
 use std::sync::Arc;
+use std::sync::Mutex;
 use std::thread;
 use std::time::Duration;
 
+//#[derive(Debug, Copy, Clone)]
 struct JobStatus {
     jobs_completed: u32,
 }
 
 fn main() {
-    let status = Arc::new(JobStatus { jobs_completed: 0 });
+    let status = Arc::new(Mutex::new(JobStatus { jobs_completed: 0}));
     let status_shared = status.clone();
+
     thread::spawn(move || {
         for _ in 0..10 {
             thread::sleep(Duration::from_millis(250));
-            status_shared.jobs_completed += 1;
+            //status_shared.jobs_completed += 1;
+            let mut num = status_shared.lock().unwrap();
+            num.jobs_completed += 1;
         }
     });
-    while status.jobs_completed < 10 {
+    while status.lock().unwrap().jobs_completed < 10 {
         println!("waiting... ");
         thread::sleep(Duration::from_millis(500));
     }
